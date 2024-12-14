c24009d1f8bd766c805aac30e8e56d8bcf1b299e
carozamani <carozamani@proton.me>
2024-12-14 05:51:53 +0330

    end total code

--- assets/sign in.js
+++ assets/sign in.js
@@ -17,18 +17,35 @@ signupLabel.addEventListener("click", function () {
 
 // "بخش دسکتاپ"//////////////////////////////////////////////
 
+// بخش چپ کانتینر
 
+const signupDescription = document.querySelector('.signup-description');
+const loginDescription = document.querySelector('.login-description');
+
+signupDescription.innerHTML = '<span><span style="font-size: 40px; font-weight: bold;">Hello</span><span style="font-size: 20px; font-weight: bold;"> there,</span><br><span>if you are not a member of our family yet, you can join our big family by entering your information.</span>';
+
+loginDescription.innerHTML = '<span><span style="font-size: 40px; font-weight: bold;">Hello</span><span style="font-size: 20px; font-weight: bold;"> friend,</span><br> you are already a member of our family and you can log in to your account by entering your email and password.</span>';
+
+
+// صفحه هاس لاگین و ساین اپ 
 const checkLoginLink = document.querySelector('.check-login');
 const orSignupLink = document.querySelector('.or-Signup');
 const loginSection = document.querySelector('.login');
 
 checkLoginLink.addEventListener('click', function() {
     loginSection.classList.toggle('active');
+    signupDescription.classList.add('sign-active')
+    loginDescription.classList.remove('log-active')
+
 });
 
 orSignupLink.addEventListener('click', function() {
     loginSection.classList.toggle('active');
+    loginDescription.classList.add('log-active')
+    signupDescription.classList.remove('sign-active')
 });
+
+// کلیک ها روی لینک و رفتار در سکشن چپ
 
 
 // حذف پیغام پیشفرض فرم///////////////////
